91e6682786357701657e4be3dfc4252f8a5c44a4
joshbam-joshwilly <Alanajoshua535@gmail.com>
2023-07-11 16:02:38 +0200

    Flatlist added

--- notjustfriends/App.js
+++ notjustfriends/App.js
@@ -1,13 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, View, } from 'react-native';
+import { StyleSheet, View, FlatList } from 'react-native';
 import FeedPost from './src/components/FeedPost';
+import posts from './assets/data/posts.json'; 
 
-
-export default function App() {
+export default function App() { 
   return (
     <View style={styles.container}>
-  
-      <FeedPost />
+      <FlatList
+        data={posts}
+        renderItem={(item) => <FeedPost post={item} />}
+      
+      />
+      
+
       <StatusBar style="auto" />
     </View>
   );
@@ -16,7 +21,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'lightgray',
     alignItems: 'center',
     justifyContent: 'center',
   },
